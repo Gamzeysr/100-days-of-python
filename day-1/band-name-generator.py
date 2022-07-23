2c0cea6d25149456a0a4ad931cef09daba7347e8
Gamze <gamzeyasar35@icloud.com>
2022-07-23 16:24:20 +0300

    pyhton

--- day-1/band-name-generator.py
+++ day-1/band-name-generator.py
@@ -3,3 +3,6 @@ street = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print("Your band name could be " + street + " " + pet)
 # Band name generator project
+
+gh bc
+print("hello world")
